fa06963c3d6b62c0d200cd004f35a410fa8a0678
ZZZank <3410764033@qq.com>
2024-12-24 23:53:08 +0800

    bare-bone for 'after' property processing

--- src/main/java/me/fengming/openjs/script/file/SortableScripts.java
+++ src/main/java/me/fengming/openjs/script/file/SortableScripts.java
@@ -1,8 +1,11 @@
 package me.fengming.openjs.script.file;
 
+import me.fengming.openjs.OpenJS;
 import me.fengming.openjs.script.ScriptProperty;
 import me.fengming.openjs.utils.Cast;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
@@ -16,7 +19,7 @@ import java.util.stream.Collectors;
 public class SortableScripts {
     private final List<SortableScript> sortables;
     private final Path base;
-    private boolean inited;
+    private boolean init;
 
     public SortableScripts(List<ScriptFile> files, Path base) {
         sortables = files.stream().map(SortableScript::new).toList();
@@ -24,8 +27,8 @@ public class SortableScripts {
     }
 
     public List<SortableScript> collect() {
-        if (!inited) {
-            inited = true;
+        if (!init) {
+            init = true;
             fromPriority();
             fromPropertyAfter(base);
         }
@@ -39,18 +42,48 @@ public class SortableScripts {
         for (var sortable : sortables) {
             var afters = sortable.file.getProperties().getOrDefault(ScriptProperty.AFTER);
             for (var after : afters) {
-                sortable.dependencies.addAll(dependenciesFromAfter(after));
+                sortable.dependencies.addAll(dependenciesFromAfter(base, after));
             }
         }
     }
 
-    private Collection<SortableScript> dependenciesFromAfter(String after) {
-        /*
+    private Collection<SortableScript> dependenciesFromAfter(Path base, String after) {
+        /*TODO
         aaa/bbb -> depends on aaa/bbb.js
         aaa/* -> depends on all files in aaa/
         aaa/someInvalidFile -> ignore and warn about it
          */
+        var parts = after.split("/");
+        validateParts(parts);
+        for (var part : parts) {
+            if ("*".equals(part)) {
+                try {
+                    Files.walk(base); //TODO
+                } catch (IOException e) {
+                    return Collections.emptyList();
+                }
+            }
+            base = base.resolve(part);
+            if (!Files.exists(base)) {
+                //error per script type
+                OpenJS.LOGGER.error(
+                    "'after' property '{}' cannot be resolved to any actual script file, ignoring",
+                    after
+                );
+                return Collections.emptyList();
+            }
+        }
         return Collections.emptyList();
+    }
+
+    private void validateParts(String[] parts) {
+        for (int i = 0; i < parts.length; i++) {
+            var part = parts[i];
+            if ("*".equals(part) && i != parts.length - 1) {
+                throw new IllegalArgumentException(
+                    "wildcard match '*' should only be the last part of a 'after' property");
+            }
+        }
     }
 
     private void fromPriority() {
